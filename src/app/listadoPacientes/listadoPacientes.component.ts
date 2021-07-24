346f578ed76a1411e7ff07286e12b7a14f0be48b
Valentina Bautista <valentina_bautista@hotmail.com>
2021-07-23 23:56:04 -0500

    Inicio del FrontEnd del proyecto

---
+++ src/app/listadoPacientes/listadoPacientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listadoPacientes',
+  templateUrl: './listadoPacientes.component.html',
+  styleUrls: ['./listadoPacientes.component.css']
+})
+export class TableListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
